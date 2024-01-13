8f6189da911a4b9d06d6c3254c70e2bb75248870
mouayed-keziz <m_keziz@estin.dz>
2024-01-13 18:21:54 +0100

    add numebrs to themes, remove border

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -60,7 +60,7 @@ function CarCards() {
         <>
             {
                 [0, 1, 2].map((item, index) => (
-                    <div key={index} className="card bg-base-100 border-zinc-600 border-2 shadow-xl">
+                    <div key={index} className="card bg-base-100 border-2 shadow-xl">
                         <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                         <div className="card-body">
                             <h2 className="card-title">Shoes!</h2>
--- src/components/theme-toggle.jsx
+++ src/components/theme-toggle.jsx
@@ -37,14 +37,14 @@ export default function ThemeToggler({ setTheme }) {
     return (
         <div className="dropdown">
             <div tabIndex={0} role="button" className="btn m-1">
-                Theme
+                Theme ({themes.length})
                 <svg width="12px" height="12px" className="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
             </div>
             <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
                 {themes.map((theme, index) => (
                     <li key={index}>
                         <button name="theme-dropdown" className="theme-controller btn btn-sm btn-block btn-ghost justify-start" aria-label="Default" onClick={(e) => { e.preventDefault(); setTheme(theme) }} >
-                            {theme}
+                            {`${index + 1}- ${theme}`}
                         </button>
                     </li>
                 ))}
